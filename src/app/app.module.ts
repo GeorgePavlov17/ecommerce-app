fbb89013a3d624708abd3ecf62ab5f42340830a6
Georgi Pavlov <georgipavlov17@gmail.com>
2022-11-30 20:26:18 +0200

    new files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,65 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {Dialog, DialogModule} from "primeng/dialog";
+import {ConfirmDialog, ConfirmDialogModule} from "primeng/confirmdialog";
+import {ConfirmationService, Footer, Header, SharedModule} from "primeng/api";
+import {TableModule} from "primeng/table";
+import {DropdownModule} from "primeng/dropdown";
+import {MenubarModule} from "primeng/menubar";
+import {ButtonModule} from "primeng/button";
+import {ListboxModule} from "primeng/listbox";
+import {RadioButtonModule} from "primeng/radiobutton";
+import {PanelModule} from "primeng/panel";
+import {AccordionModule} from "primeng/accordion";
+import {CalendarModule} from "primeng/calendar";
+import {TabViewModule} from "primeng/tabview";
+import {FocusTrapModule} from "primeng/focustrap";
+import {CheckboxModule} from "primeng/checkbox";
+import {TreeTableModule} from "primeng/treetable";
+import {TreeModule} from "primeng/tree";
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    // Dialog,
+    // ConfirmDialog,
+    // Header,
+    // Footer
+
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    SharedModule,
+    TableModule,
+    DialogModule,
+    ConfirmDialogModule,
+    DropdownModule,
+    MenubarModule,
+    ButtonModule,
+    ListboxModule,
+    RadioButtonModule,
+    PanelModule,
+    AccordionModule,
+    CalendarModule,
+    TabViewModule,
+    FocusTrapModule,
+    CheckboxModule,
+    TreeTableModule,
+    TreeModule
+
   ],
-  providers: [],
+  providers: [
+    ConfirmationService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
